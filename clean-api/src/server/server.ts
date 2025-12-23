cef41c0116c034d487ce2020fe830c283ee050ab
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-23 14:51:18 -0600

    feat: import router to server

--- clean-api/src/server/server.ts
+++ clean-api/src/server/server.ts
@@ -1,10 +1,14 @@
 import {Elysia } from 'elysia'
+import { userRouter } from '../user/userRouter'
 
 export class Server {
     private app: Elysia
     
     constructor() {
         this.app = new Elysia()
+        this.app.group("/api/v1", (app) => {
+            return app.use(userRouter)
+        })
     }
     public start(){
         this.app.listen(Bun.env.PORT || 3000, () => {
