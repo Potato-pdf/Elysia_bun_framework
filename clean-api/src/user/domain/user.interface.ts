96b14cb3224802b5edff21ba5a698ca447527348
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-24 10:50:49 -0600

    feat: user interface

---
+++ clean-api/src/user/domain/user.interface.ts
@@ -0,0 +1,9 @@
+import { User } from "./user";
+
+export interface UserInterface {
+    create(email: string, password: string, name?: string): Promise<User>;
+    findByEmail(email: string): Promise<User | null>;
+    findById(id: string): Promise<User | null>;
+    update(user: User): Promise<User>;
+    delete(id: string): Promise<void>;
+}
