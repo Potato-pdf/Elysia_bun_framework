810c3239e4da785be43b215dfe4497aacf6e1370
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-26 12:24:10 -0600

    feat: implement user dao in repository

--- clean-api/src/user/domain/user.repository.ts
+++ clean-api/src/user/domain/user.repository.ts
@@ -1,13 +1,23 @@
 import { User } from "./user";
-import { UserInterface } from "./user.interface";
 import { PrismaClient } from "../../generated/prisma/client";
+import { UserDAOInterface } from "./interfaces/user.dao.interface";
+import { UserInterface } from "./interfaces/user.interface";
 
 
 
 export class UserRepository implements UserInterface{
-        constructor(private db: PrismaClient) {}
+    constructor(private usuarioDAO : UserDAOInterface){}
     async create(email: string, password: string, name?: string): Promise<User> {
+        const nuevoUsuario = new User(
+            crypto.randomUUID(),  // ← Genera ID
+            email,
+            password,
+            name,
+            new Date()
+        );
+        return await this.usuarioDAO.create(nuevoUsuario)
         throw new Error("Method not implemented.")
+
     }
     async findByEmail(email: string): Promise<User | null> {
         throw new Error("Method not implemented.")
