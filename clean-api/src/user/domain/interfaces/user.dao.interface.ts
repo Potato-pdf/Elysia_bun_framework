318d0e91b1f12c66e87032f7712e90d831ee4706
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-26 14:28:04 -0600

    feat: implemente dto to dao

--- clean-api/src/user/domain/interfaces/user.dao.interface.ts
+++ clean-api/src/user/domain/interfaces/user.dao.interface.ts
@@ -1,10 +1,11 @@
 import { User } from "../user";
-
+import { CreateUserDTO } from "../dto/createUser.dto";
+import { UpdateUserDTO } from "../dto/updateUser.dto";
 
 export interface UserDAOInterface {
-    create(usuario: User): Promise<User>;
+    create(data: CreateUserDTO): Promise<User>;
     findByEmail(email: string): Promise<User | null>;
     findById(id: string): Promise<User | null>;
-    update(user: User): Promise<User>;
+    update(data: UpdateUserDTO): Promise<User>;
     delete(id: string): Promise<void>;
 }
