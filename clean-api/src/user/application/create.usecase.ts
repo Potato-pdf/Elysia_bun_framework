dc95a65c4506c5919d240790e10e628b66bc6722
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-26 14:50:40 -0600

    feat: create user use case

---
+++ clean-api/src/user/application/create.usecase.ts
@@ -0,0 +1,24 @@
+import { UserInterface } from "../domain/interfaces/user.interface";
+
+export class CreateUserUSeCase{
+    constructor(
+        private userRepository : UserInterface
+    ){}
+    async run (email:string, password: string, name:string){
+        if (!email.includes('@')) {
+            throw new Error('Email inválido');
+            }
+        
+        if (password.length < 8) {
+            throw new Error('Password debe tener mínimo 8 caracteres');
+            }
+        const existingUser = await this.userRepository.findByEmail(email);
+        if (existingUser) {
+            throw new Error('El email ya está registrado');
+        }
+        
+        const hashedPassword = await Bun.password.hash(password);
+
+        return await this.userRepository.create(email, hashedPassword, name);
+    }
+}
