a7ab03bbf079e9e5756c842a7049ea9949b0baa6
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-26 14:27:50 -0600

    IMPLEMENTE dto TO REPOSITORY

--- clean-api/src/user/domain/user.repository.ts
+++ clean-api/src/user/domain/user.repository.ts
@@ -2,20 +2,22 @@ import { User } from "./user";
 import { PrismaClient } from "../../generated/prisma/client";
 import { UserDAOInterface } from "./interfaces/user.dao.interface";
 import { UserInterface } from "./interfaces/user.interface";
-
-
+import { CreateUserDTO } from "./dto/createUser.dto";
+import { UpdateUserDTO } from "./dto/updateUser.dto";
 
 export class UserRepository implements UserInterface{
     constructor(private usuarioDAO : UserDAOInterface){}
+    
     async create(email: string, password: string, name?: string): Promise<User> {
-        const nuevoUsuario = new User(
-            crypto.randomUUID(),  // Genera el ID
+        // Creamos el DTO sin id ni createdAt
+        const createUserDTO: CreateUserDTO = {
             email,
             password,
-            name,
-            new Date()  // Fecha de creación
-        );
-        return await this.usuarioDAO.create(nuevoUsuario)
+            name
+        };
+        
+        // El DAO envía a Prisma, que genera id y createdAt automáticamente
+        return await this.usuarioDAO.create(createUserDTO);
     }
     async findByEmail(email: string): Promise<User | null> {
         return await this.usuarioDAO.findByEmail(email);
@@ -24,9 +26,17 @@ export class UserRepository implements UserInterface{
         return await this.usuarioDAO.findById(id);
     }
     async delete(id: string): Promise<void> {
-        throw new Error("Method not implemented")
+        return await this.usuarioDAO.delete(id)
     }
     async update(user: User): Promise<User> {
-        throw new Error("Method not implemented")
+        // Creamos el DTO con los datos a actualizar
+        const updateUserDTO: UpdateUserDTO = {
+            id: user.id,
+            email: user.email,
+            password: user.password,
+            name: user.name
+        };
+        
+        return await this.usuarioDAO.update(updateUserDTO);
     }
 }
