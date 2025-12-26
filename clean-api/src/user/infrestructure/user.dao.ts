4cfcbd827fc505589a56808809278c7e6a5034e9
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-26 12:26:23 -0600

    fix: user DAO

--- clean-api/src/user/infrestructure/user.dao.ts
+++ clean-api/src/user/infrestructure/user.dao.ts
@@ -1,19 +1,20 @@
 import { PrismaClient } from "../../generated/prisma/client";
 import { User } from "../domain/user";
-import { UserInterface } from "../domain/user.interface";
+import { UserDAOInterface } from "../domain/interfaces/user.dao.interface";
 
 
 
-export class UsuarioDAO implements UserInterface {
+export class UsuarioDAO implements UserDAOInterface {
     constructor(private db: PrismaClient) {}
-    async create(email: string, password: string, name?: string): Promise<User> {
-       const createdUser = await this.db.user.create({
+    
+    async create(usuario: User): Promise<User> {
+        const createdUser = await this.db.user.create({
             data: {
-                email:email,
-                password:password,
-                name:name
+                email: usuario.email,
+                password: usuario.password,
+                name: usuario.name
             }
         });
-        return createdUser as User
+        return createdUser as User;
     }
 }
