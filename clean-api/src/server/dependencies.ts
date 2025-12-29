32b4aac89b5c1e22302c89b87144067dffbb317f
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-29 12:56:52 -0600

    add hash to dependencies

--- clean-api/src/server/dependencies.ts
+++ clean-api/src/server/dependencies.ts
@@ -1,11 +1,10 @@
 import { UserRepository } from "../user/domain/user.repository";
 import { CreateUserUSeCase } from "../user/application/create.usecase";
 import { CreateUserController } from "../user/infrestructure/controllers/create.controller";
-import { UsuarioDAO } from "../user/infrestructure/user.dao";
-import { PrismaClient } from "../generated/prisma/client";
+import { Hash } from "../services/hashing-service";
 
-const prisma = new PrismaClient();
-const userDAO = new UsuarioDAO(prisma);
-const userRepository = new UserRepository(userDAO);
-const createUser = new CreateUserUSeCase(userRepository);
+
+const userRepository = new UserRepository();
+const hashService = new Hash();
+const createUser = new CreateUserUSeCase(userRepository, hashService);
 export const createUserController = new CreateUserController(createUser);
