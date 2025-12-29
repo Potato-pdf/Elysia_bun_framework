30dac49119054bba42e7b8302b7596ca2140c936
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-29 11:47:22 -0600

    feat: implemente controller to router

--- clean-api/src/user/userRouter.ts
+++ clean-api/src/user/userRouter.ts
@@ -1,6 +1,13 @@
-import Elysia from "elysia";
+import {Elysia, t }from "elysia";
+import { createUserController } from "../server/dependencies"; 
+import { password } from "bun";
 
 export const userRouter = new Elysia({prefix: "/users"})
     .get("/", () => "User list")
     .get("/:id", ({ params }) => `User with ID: ${params.id}`)
-    .post("/create", () => "User created");
+    .post("/create", createUserController.run.bind(createUserController),{body : t.Object({
+        email: t.String(),
+        password: t.String(),
+        name: t.String(),
+    })
+});
