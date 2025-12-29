6bd8e7779c0cf1a730d0dcab77a64897b64b10c7
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-29 12:58:53 -0600

    feat: implemente DTO to routes

--- clean-api/src/user/userRouter.ts
+++ clean-api/src/user/userRouter.ts
@@ -1,9 +1,8 @@
 import {Elysia, t }from "elysia";
 import { createUserController } from "../server/dependencies"; 
-import { password } from "bun";
+import { CreateUserDTO } from "./domain/dto/createUser.dto";
 
 export const userRouter = new Elysia({prefix: "/users"})
     .get("/", () => "User list")
     .get("/:id", ({ params }) => `User with ID: ${params.id}`)
-    .post("/create", createUserController.run.bind(createUserController),{k
-});
+    .post("/create", createUserController.run.bind(createUserController),CreateUserDTO);
