95e7f64cf90320b29b931491bbc45757b9b01542
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-29 12:56:38 -0600

    delete body from routes

--- clean-api/src/user/userRouter.ts
+++ clean-api/src/user/userRouter.ts
@@ -5,9 +5,5 @@ import { password } from "bun";
 export const userRouter = new Elysia({prefix: "/users"})
     .get("/", () => "User list")
     .get("/:id", ({ params }) => `User with ID: ${params.id}`)
-    .post("/create", createUserController.run.bind(createUserController),{body : t.Object({
-        email: t.String(),
-        password: t.String(),
-        name: t.String(),
-    })
+    .post("/create", createUserController.run.bind(createUserController),{k
 });
