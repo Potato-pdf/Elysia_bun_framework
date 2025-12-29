4b9dc9d4d55f0e04e73949e3a69315946863a8dc
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-29 09:55:16 -0600

    feat: dependencies

---
+++ clean-api/src/server/dependencies.ts
@@ -0,0 +1,3 @@
+import { UserRepository } from "../user/domain/user.repository";
+import { CreateUserUSeCase } from "../user/application/create.usecase";
+import { CreateUserController } from "../user/infrestructure/controllers/create.controller";
