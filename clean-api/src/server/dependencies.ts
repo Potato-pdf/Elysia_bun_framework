04adc9beb4cc7af552bf69f9f9366997ca2a909f
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-12-29 11:36:52 -0600

    feat: dependencies

--- clean-api/src/server/dependencies.ts
+++ clean-api/src/server/dependencies.ts
@@ -1,3 +1,11 @@
 import { UserRepository } from "../user/domain/user.repository";
 import { CreateUserUSeCase } from "../user/application/create.usecase";
 import { CreateUserController } from "../user/infrestructure/controllers/create.controller";
+import { UsuarioDAO } from "../user/infrestructure/user.dao";
+import { PrismaClient } from "../generated/prisma/client";
+
+const prisma = new PrismaClient();
+const userDAO = new UsuarioDAO(prisma);
+const userRepository = new UserRepository(userDAO);
+const createUser = new CreateUserUSeCase(userRepository);
+export const createUSerCOntroller = new CreateUserController(createUser);
